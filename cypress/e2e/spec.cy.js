bb5f6e1189a9fc059a956ef9892737c81d881f31
Tatyana Belyaeva <tat.bor.belyaeva@gmail.com>
2024-07-02 15:24:13 +0300

    Add auto test for positive scenario

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,29 @@
+import 'cypress-file-upload';
+
+describe('template spec', () => {
+  it('Visits the page and checks for an element', () => {
+    // Откройте страницу:
+    cy.visit('https://js-55fbfg.stackblitz.io/');
+
+    // Нажмите кнопку "Run this project"
+    cy.contains('Run this project').click();
+
+    // Ждем, пока загрузится новая страница
+    cy.wait(5000); // Ждать 5 секунд для загрузки новой страницы
+
+    // Нажмите кнопку "Добавить"
+    cy.get('button').contains('Добавить').click();
+
+    // Убедитесь, что элемент для загрузки файла виден
+    cy.get('.card.dropbox-panel').should('be.visible');
+
+    // Имитация перетаскивания файла
+    const filePath = '.\\cypress\\e2e\\test_certs\\privat_2018.cer'; // Относительный путь к файлу в папке fixtures
+
+    // Перетащить файл в элемент с классом "dropbox-panel"
+    cy.get('.dropbox').selectFile(filePath, {action:'drag-drop'});
+
+    // Проверить, что файл появился в div с тегом a
+    cy.get('div a').contains('UA-14360570-2018').should('be.visible');
+  });
+});
